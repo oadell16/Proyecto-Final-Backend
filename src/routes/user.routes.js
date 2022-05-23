437a83296da4637b080d969ed499a240f32381a5
Oscar Adell <oadell@ibadia.cat>
2022-05-23 20:38:41 +0200

    comentarios

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,13 +6,17 @@ const router = express.Router();
 
 // Display all users
 router.get('/api/users', userController.getUsers);
+
 //display user by id
 router.get('/api/users/:id', userController.getUser);
 
+//add user to database
 router.post('/api/users', userController.addUser);
 
+//delete user by id
 router.delete('/api/users/:id', userController.deleteUser);
 
+//update user by id and data in body
 router.put('/api/users/:id', userController.updateUser);
 
 
