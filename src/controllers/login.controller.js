78a72e5123b287ff9b2c92f5c6df8844fc44a251
Alex <alexgallardoescobar@gmail.com>
2022-05-24 23:35:54 +0200

    fix del send

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -6,7 +6,10 @@ loginController.login = (req, res) => {
     let query = `select * from gestbot_users where username = '${req.query.username}' or user_email = '${req.query.username}'`;
     pool.query(query, async (error, result) => {
         if (error) throw error;
-        if (result.rowCount < 1) res.send("No se encontro al usuario");
+        if (result.rowCount < 1){
+            res.send("No se encontro al usuario");
+            return
+        } 
         let bcresult = await bcrypt.compare(result.rows[0].user_password, req.query.password)
         console.log(bcresult);
 
@@ -15,6 +18,7 @@ loginController.login = (req, res) => {
                 status: true,
                 msg: "OK"
             })
+
         } else {
             res.send({
                 status: false,
@@ -22,8 +26,6 @@ loginController.login = (req, res) => {
             })
         }
     });
-    console.log(req.query);
-    res.send("me llega")
 }
 
 module.exports = loginController;
